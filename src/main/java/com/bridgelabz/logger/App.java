9ffbcdf07a7737af701aec47cab1aa290563dc22
datta1808 <bagaledatta18@gmail.com>
2021-05-18 11:18:40 +0530

    added logging in app class

--- src/main/java/com/bridgelabz/logger/App.java
+++ src/main/java/com/bridgelabz/logger/App.java
@@ -1,13 +1,35 @@
 package com.bridgelabz.logger;
 
+import java.io.IOException;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.SimpleLayout;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	static Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+    	// here Appender is an interface
+//		Layout layout = new SimpleLayout();
+//		Layout layout = new HTMLLayout();
+		Layout layout = new PatternLayout("%d %p %C %M %m %n"); //date, Class, Method, message, newline
+//		Appender appender = new ConsoleAppender(layout); // appender is used for destination ex. Console using consoleAppender
+		Appender appender = new FileAppender(layout, "Applog.log");
+		logger.addAppender(appender); // to add appender to the logger
+		logger.debug("debug message");
+		logger.info("info message");
+		logger.warn("warn message");
+		logger.error("error message");
+		logger.fatal("fatal message");
     }
 }
